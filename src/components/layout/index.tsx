8778e13cd8fc0d818bab8c8fc03d7a3217262476
momarip <momarip.el.yousfi@gmail.com>
2023-10-14 14:18:53 +0100

    layout, header and sidebar

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,14 @@
+import Header from "./header";
+import Sidebar from "./sidebar";
+
+const Layout = () => {
+    return (
+      <div>
+        <Header />
+        <Sidebar />
+      </div>
+    );
+  };
+  
+  export default Layout;
+  
---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <aside
+      id="default-sidebar"
+      className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+      aria-label="Sidebar"
+    >
+      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+        <ul className="space-y-2 font-medium">
+          <li>
+            <a
+              href="#"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <span className="ml-3">Books</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            >
+              <span className="ml-3">Members</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
