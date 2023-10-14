f3014a7b6338a14f38c58518399058e5dfc7d8c6
abdenassar <abdenassaramimi99@gmail.com>
2023-10-14 17:04:44 +0100

    adding route for adding book

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,25 +2,29 @@ import { createBrowserRouter } from "react-router-dom";
 import Signin from "./app/features/authentication/components/signin";
 import RootLayout from "./pages/panel/root-layout";
 import BooksList from "./app/features/bookList/components/booksList";
+import Book from "./app/features/book/book";
 
 export const router = createBrowserRouter([
-  {
-    index: true,
-    element: <p>This is shop </p>,
-  },
-  {
-    path: "/signin",
-    element: <Signin />,
-  },
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/books",
-        element: <BooksList />,
-      },
-      
-    ],
-  },
+	{
+		index: true,
+		element: <p>This is shop </p>,
+	},
+	{
+		path: "/signin",
+		element: <Signin />,
+	},
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{
+				path: "/books",
+				element: <BooksList />,
+			},
+			{
+				path: "/books/add",
+				element: <Book />,
+			},
+		],
+	},
 ]);
